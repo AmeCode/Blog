cba5ee03b41b15bc66a034bdba90a741a2473a4f
unknown <ahmed_amir30@icloud.com>
2018-08-08 06:40:34 +0200

    test

--- app.js
+++ app.js
@@ -15,14 +15,13 @@ mongoose.connection.on('error',()=>console.log('error connecting to the database
 app.listen(port,()=>{
   console.log("connected on port "+port)
 });
-
+app.use(express.static(path.join(__dirname,'/public')));
 app.get('*',(req,res)=>{
-  console.log(path.join(__dirname,'public/index.html'))
+  res.sendFile(path.join(__dirname,'/public/index.html'))
 });
 app.use(cors());
 app.use(bodyparser.json());
 app.use('/users',users);
-app.use(express.static(path.join(__dirname,'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
