7c83aa2c2e89fb2ea757c068707e2058b49adb78
unknown <ahmed_amir30@icloud.com>
2018-08-08 15:26:01 +0200

    Revert to 736e2bc3b007d997cf1f14c9c4eb6029e167d10a

--- app.js
+++ app.js
@@ -8,20 +8,23 @@ const mongoose = require('mongoose');
 const users = require('./routes/users');
 const CircularJSON = require('circular-json');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 mongoose.connect(config.database,{useNewUrlParser:true});
 mongoose.connection.on('connected',()=>console.log('connected to database!'));
 mongoose.connection.on('error',()=>console.log('error connecting to the database!!'));
 app.listen(port,()=>{
   console.log("connected on port "+port)
 });
-app.use(express.static(path.join(__dirname, './public')));
-app.get('/*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'./public','index.html'))
+app.get('/',(req,res)=>{
+  res.send('connected to \/')
+});
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/index.html'))
 });
 app.use(cors());
 app.use(bodyparser.json());
 app.use('/users',users);
+app.use(express.static(path.join(__dirname,'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-  database:"mongodb://ultimate_medo:Ultimate3031@ds113692.mlab.com:13692/akraat",
-  secret: 'silkroad'
-};
